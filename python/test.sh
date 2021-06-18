33ec1a422f2383832f8034c8a98ca4375651f6cd
tyaromax <tkentyan@hotmail.com>
2021-06-18 22:17:12 +0900

    Update test.sh

--- python/test.sh
+++ python/test.sh
@@ -2,5 +2,9 @@
 now=$(date +%Y%m%d%H%M)
 echo $now
 
-sed -i -e "s/version=.*\"/version=$now\"/g" $HOME/kousotsu-py.info/public_html/dist/js/crypto_table2.js
-sed -i -e "s/js?version=.*\"/js?version=$now\"/g" $HOME/kousotsu-py.info/public_html/index.html
+sed -i -e "s/version=.*\"/version=$now\"/g" ./public_html/dist/js/crypto_info.js
+sed -i -e "s/js?version=.*\"/js?version=$now\"/g" ./public_html/index.html
+sed -i -e "s/version=.*\"/version=$now\"/g" ./public_html/dist/js/crypto_info_result.js
+sed -i -e "s/js?version=.*\"/js?version=$now\"/g" ./public_html/index2.html
+sed -i -e "s/version=.*\"/version=$now\"/g" ./public_html/dist/js/crypto_info_star.js
+sed -i -e "s/js?version=.*\"/js?version=$now\"/g" ./public_html/star.html
